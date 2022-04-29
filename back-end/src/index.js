63be5b254246c548415f7d300a502cb05bdac392
Shahbaz Ali <engr.shahbaz92@gmail.com>
2022-04-29 11:18:28 +0300

    node project created

---
+++ back-end/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
